7d6a8f52aafe96d7b5fba68c9dbec6f286ccf874
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-06-17 19:30:51 +0600

    Handling arrays

--- Part 1 - Introduction to React/complex-state-and-debugging/src/App.js
+++ Part 1 - Introduction to React/complex-state-and-debugging/src/App.js
@@ -4,12 +4,17 @@ const App = () => {
   const [clicks, setClicks] = useState({
     left: 0, right: 0
   })
+  const [allClicks, setAll] = useState([])
 
-  const handleLeftClick = () =>
-  setClicks({ ...clicks, left: clicks.left + 1 })
+  const handleLeftClick = () => {
+    setClicks({ ...clicks, left: clicks.left + 1 })
+    setAll(allClicks.concat('L'))
+  }
 
-  const handleRightClick = () =>
+  const handleRightClick = () => {
     setClicks({ ...clicks, right: clicks.right + 1 })
+    setAll(allClicks.concat('R'))
+  }
 
   return (
     <div>
@@ -17,6 +22,7 @@ const App = () => {
       <button onClick={handleLeftClick}>left</button>
       <button onClick={handleRightClick}>right</button>
       {clicks.right}
+      <p>{allClicks.join(' ')}</p>
     </div>
   )
 }
