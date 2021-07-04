37d989a959540b8c408b161c0f131cb0ab20ccd3
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-07-05 00:31:14 +0600

    2.5: separate module

--- Part 2 - Communicating with server/courseinfo/src/App.js
+++ Part 2 - Communicating with server/courseinfo/src/App.js
@@ -1,46 +1,7 @@
 import React from 'react'
+import Course from './components/course'
 
-const Course = ({ course }) => {
-  return (
-    <div>
-      <Header course={course} />
-      <Content course={course} />
-      <Total course={course} />
-    </div>
-  )
-}
 
-const Header = ({ course }) => {
-  return (
-    <h1>{course.name}</h1>
-  )
-}
-
-const Total = ({ course }) => {
-  const sum = course.parts.map(course => course.exercises)
-  .reduce((accumulator, currentValue) => accumulator + currentValue, 0)
-  return (
-    <p><strong>total of {sum} exercises</strong></p>
-  ) 
-}
-
-const Part = ({ part }) => {
-  return (
-    <p>
-      {part.name} {part.exercises}
-    </p>    
-  )
-}
-
-const Content = ({ course }) => {
-  return (
-    <div>
-      {course.parts.map(part => 
-        <Part key={part.id} part={part} />
-      )}
-    </div>
-  )
-}
 
 const App = () => {
   const courses = [
