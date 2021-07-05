b945f6938f30ac1ac73637a90782b4f227c10794
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-07-05 19:13:34 +0600

    2.8: The Phonebook Step3

--- Part 2 - Communicating with server/phonebook/src/App.js
+++ Part 2 - Communicating with server/phonebook/src/App.js
@@ -2,23 +2,30 @@ import React, { useState } from "react";
 import Person from './components/Persons'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
-  const [newName, setNewName] = useState("");
+  const [ persons, setPersons ] = useState([
+    { name: 'Arto Hellas', number: '39-44-5323523' }
+  ]) 
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
 
   const handleAddName = (event) => {
+    let oldPersons = [...persons];
     event.preventDefault();
-    if (
-      persons
-        .map(element => element.name.toLowerCase())
-        .includes(newName.toLowerCase())
-    ) {
-      window.alert(`${newName} is already added to phonebook`);
-    } else {
-      let oldPersons = [...persons];
-      oldPersons.push({ name: newName });
-      setPersons(oldPersons);
-      setNewName("");
+    if (!newName || !newNumber) {
+      alert("Name or Number can not be empty");
+      return;
     }
+    if (oldPersons.map((person) => person.name.toLowerCase()).includes(newName.toLowerCase().trim())) {
+
+      window.alert(`${newName.trim()} is already added to phonebook`);
+      return;
+    } 
+  
+    oldPersons.push({ name: newName, number: newNumber });
+    setPersons(oldPersons);
+    setNewName("");
+    setNewNumber("");
+    
   };
 
   return (
@@ -32,13 +39,23 @@ const App = () => {
             onChange={(event) => setNewName(event.target.value)}
           />
         </div>
+        
+        <div> 
+        number: 
+        <input
+            value={newNumber}
+            onChange={(event) => setNewNumber(event.target.value)}
+          />
+        </div>
         <div>
           <button type="submit">add</button>
         </div>
       </form>
+      { /*<div>debug: {newName}</div> */}
       <h2>Numbers</h2>
       {persons.map((person) => (
-        <Person key={person.id} person={person}/>
+        <Person key={person.name} 
+        person={person}/>
       ))
       }
     </div>
