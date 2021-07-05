b21fb1f9000847fef30f05e5c7f2a5bda8c69d2a
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-07-05 16:18:20 +0600

    2.6: The Phonebook Step1

---
+++ Part 2 - Communicating with server/phonebook/src/App.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+
+const App = () => {
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" }]);
+  const [newName, setNewName] = useState("");
+
+  const handleAddName = (event) => {
+    event.preventDefault();
+    let oldPersons = [...persons];
+    oldPersons.push({ name: newName });
+    setPersons(oldPersons);
+    setNewName("");
+
+  };
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleAddName}>
+        <div>
+          name:
+          <input
+            value={newName}
+            onChange={(event) => setNewName(event.target.value)}
+          />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person) => (
+        <div>{person.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default App;
