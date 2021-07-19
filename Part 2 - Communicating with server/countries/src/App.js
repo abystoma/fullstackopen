8c37c62341dc36b36f3808a8461193a48f4ab0ce
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-07-19 17:02:46 +0600

    2.13*: Data for countries, step2

--- Part 2 - Communicating with server/countries/src/App.js
+++ Part 2 - Communicating with server/countries/src/App.js
@@ -35,7 +35,7 @@ const App = () => {
       handleSearch={handleSearch}
       search={search} handleSearchChange={handleSearchChange}
       />
-      <Countries countries={countries} search={search} />
+      <Countries countries={countries} search={search}  setSearch={setSearch} />
     </div>
   );
 };
