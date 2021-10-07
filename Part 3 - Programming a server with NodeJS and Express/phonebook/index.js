567e6527b63ea33865374f2d52fb6a0b3d27e992
flukehermit <zayed.humayun@g.bracu.ac.bd>
2021-10-08 00:59:15 +0600

    3.16: Phonebook database, step4

--- Part 3 - Programming a server with NodeJS and Express/phonebook/index.js
+++ Part 3 - Programming a server with NodeJS and Express/phonebook/index.js
@@ -29,7 +29,9 @@ app.get('/api/persons/:id', (request, response, next) => {
         response.status(404).end()
       }
     })
-    .catch(error => next(error))
+    
+    .catch(error => 
+      next(error))
 })
 app.get('/info', (request, response) => {
     Person.find({}).then(persons => {
@@ -42,14 +44,15 @@ app.get('/info', (request, response) => {
     })
   })
   
-app.delete('/api/persons/:id', (request, response) => {
+app.delete('/api/persons/:id', (request, response, next) => {
   Person.findByIdAndRemove(request.params.id)
     .then(result => {
       response.status(204).end()
     })
+    .catch(error => next(error))
   })
 
-app.post('/api/persons', (request, response) => {
+app.post('/api/persons', (request, response, next) => {
     const body = request.body
 
     if (!body.name || !body.number) {
@@ -76,7 +79,22 @@ app.post('/api/persons', (request, response) => {
     .then(savedAndFormattedPerson => {
       response.json(savedAndFormattedPerson)
     }) 
+    .catch(error => next(error))
+
 })
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError' && error.kind === 'ObjectId') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
