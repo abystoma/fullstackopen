37014bffd772da7c1f242e1e036e6f1cd426dbe7
flukehermit <zayed.humayun@g.bracu.ac.bd>
2022-02-04 23:16:04 +0600

    Refactored test

--- note-app/server/controllers/notes.js
+++ note-app/server/controllers/notes.js
@@ -1,27 +1,11 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
 
-notesRouter.get('/', async (request, response) => { 
+notesRouter.get('/', async (request, response) => {
   const notes = await Note.find({})
-  response.json(notes.map(note => note.toJSON()))
+  response.json(notes)
 })
-
-notesRouter.get('/:id', async (request, response, next) => {
-  try{
-    const note = await Note.findById(request.params.id)
-    if (note) {
-      response.json(note)
-    } else {
-      response.status(404).end()
-    }
-  } catch(exception) {
-    next(exception)
-  }
-})
-
-
-
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
   const body = request.body
 
   const note = new Note({
@@ -30,21 +14,61 @@ notesRouter.post('/', async (request, response, next) => {
     date: new Date(),
   })
 
-  try { 
-    const savedNote = await note.save()
-    response.json(savedNote)
-  } catch(exception) {
-    next(exception)
-  }
+  const savedNote = await note.save()
+  response.status(201).json(savedNote)
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
-  try {
-    await Note.findByIdAndRemove(request.params.id)
-    response.status(204).end()
-  } catch (exception) {
-    next(exception)
+notesRouter.get('/:id', async (request, response) => {
+  const note = await Note.findById(request.params.id)
+  if (note) {
+    response.json(note.toJSON())
+  } else {
+    response.status(404).end()
   }
+})
+// notesRouter.get('/:id', async (request, response, next) => {
+//   try{
+//     const note = await Note.findById(request.params.id)
+//     if (note) {
+//       response.json(note)
+//     } else {
+//       response.status(404).end()
+//     }
+//   } catch(exception) {
+//     next(exception)
+//   }
+// })
+
+
+
+// notesRouter.post('/', async (request, response, next) => {
+//   const body = request.body
+
+//   const note = new Note({
+//     content: body.content,
+//     important: body.important || false,
+//     date: new Date(),
+//   })
+
+//   try { 
+//     const savedNote = await note.save()
+//     response.json(savedNote)
+//   } catch(exception) {
+//     next(exception)
+//   }
+// })
+
+// notesRouter.delete('/:id', async (request, response, next) => {
+//   try {
+//     await Note.findByIdAndRemove(request.params.id)
+//     response.status(204).end()
+//   } catch (exception) {
+//     next(exception)
+//   }
+// })
+notesRouter.delete('/:id', async (request, response) => {
+  await Note.findByIdAndRemove(request.params.id)
+  response.status(204).end()
 })
 
 notesRouter.put('/:id', (request, response, next) => {
@@ -57,7 +81,7 @@ notesRouter.put('/:id', (request, response, next) => {
 
   Note.findByIdAndUpdate(request.params.id, note, { new: true })
     .then(updatedNote => {
-      response.json(updatedNote.toJSON())
+      response.json(updatedNote)
     })
     .catch(error => next(error))
 })
