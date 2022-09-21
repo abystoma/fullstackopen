1718c5fab5f34981f3d7007b4800ae3558f74805
flukehermit <zayed.humayun@g.bracu.ac.bd>
2022-09-21 21:12:33 +0600

    fixed eslint errors

--- note-app/server/.eslintrc.js
+++ note-app/server/.eslintrc.js
@@ -1,31 +1,25 @@
 module.exports = {
-  'env': {
-    'commonjs': true,
-    'es2021': true,
-    'node': true,
-    'jest': true,
+  env: {
+    commonjs: true,
+    es2021: true,
+    node: true,
   },
-  'extends': 'eslint:recommended',
-  'parserOptions': {
-    'ecmaVersion': 12
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 'latest',
   },
-  'rules': {
-    'indent': [
-      'error',
-      2
-    ],
+  rules: {
+    indent: ['error', 2],
     'linebreak-style': [
       'error',
-      'unix'
+      process.platform === 'win32' ? 'windows' : 'unix',
     ],
-    'quotes': [
-      'error',
-      'single'
-    ],
-    'semi': [
-      'error',
-      'never'
-    ],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    eqeqeq: 'error',
+    'no-trailing-spaces': 'error',
+    'object-curly-spacing': ['error', 'always'],
+    'arrow-spacing': ['error', { before: true, after: true }],
     'no-console': 0,
-  }
-}
+  },
+};
--- note-app/server/mongo.js
+++ note-app/server/mongo.js
@@ -1,31 +1,31 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
 if (process.argv.length < 3) {
-  console.log('Please provide the password as an argument: node mongo.js <password>')
-  process.exit(1)
+  console.log(
+    'Please provide the password as an argument: node mongo.js <password>'
+  );
+  process.exit(1);
 }
 
-const password = process.argv[2]
+const password = process.argv[2];
 
+const url = `mongodb+srv://flukehermit:${password}@cluster0.euxsz.mongodb.net/note-app?retryWrites=true&w=majority`;
 
-const url =
-  `mongodb+srv://flukehermit:${password}@cluster0.euxsz.mongodb.net/note-app?retryWrites=true&w=majority`
-
-mongoose.connect(url)
+mongoose.connect(url);
 
 const noteSchema = new mongoose.Schema({
   content: String,
   date: Date,
   important: Boolean,
-})
+});
 
-const Note = mongoose.model('Note', noteSchema)
+const Note = mongoose.model('Note', noteSchema);
 
 const note = new Note({
   content: 'Mongoose make use of mongo easy',
   date: new Date(),
   important: true,
-})
+});
 
 /*
 note.save().then(response => {
@@ -34,9 +34,9 @@ note.save().then(response => {
 })
 */
 
-Note.find({}).then(result => {
-  result.forEach(note => {
-    console.log(note)
-  })
-  mongoose.connection.close()
-})
+Note.find({}).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
