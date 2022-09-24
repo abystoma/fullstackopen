9b9dc91c35ce8edb6159bbc793e289268ac88dd0
flukehermit <zayed.humayun@g.bracu.ac.bd>
2022-09-24 23:33:45 +0600

    4.4: helper functions and unit tests, step2

--- bloglist/server/utils/list_helper.js
+++ bloglist/server/utils/list_helper.js
@@ -1,5 +1,11 @@
 const dummy = (blogs) => 1;
-
+const totalLikes = (blogs) => {
+  if (blogs.length === 0) {
+    return 0;
+  }
+  return blogs.reduce((sum, b) => sum + b.likes, 0);
+};
 module.exports = {
   dummy,
+  totalLikes,
 };
